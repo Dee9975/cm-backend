f967a9a7b7454a04c8df2dc873551a3e322d4403
matisskalnins6@gmail.com <9z)car)){6:L*/cq&Q\,4"EFyB,H>
2021-07-05 23:17:30 +0300

    Docker changes

--- bin/lib/client/client.dart
+++ bin/lib/client/client.dart
@@ -7,7 +7,7 @@ class Client {
 
   Future<void> main(List<String> args) async {
     channel = ClientChannel(
-      "localhost",
+      "matisskalnins-cm-backend-sq2tv.ondigitalocean.app",
       port: 50051,
       options: ChannelOptions(
         credentials: ChannelCredentials.insecure(),
